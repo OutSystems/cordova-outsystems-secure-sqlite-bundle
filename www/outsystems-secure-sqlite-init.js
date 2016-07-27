6ece1b44bcbeea3c41db16e1f9f46e93d8b142d0
Paulo Duarte Ferreira <pdf@domain.outsystems.com>
2016-07-27 16:43:56 +0100

    Clean-up plugin initialization

--- www/outsystems-secure-sqlite-init.js
+++ www/outsystems-secure-sqlite-init.js
@@ -1,53 +1,68 @@
-// force dependency
+// Force dependency load
 var SQLiteCipher = require('cordova-sqlcipher-adapter.SQLitePlugin');
 var SecureStorage = require('cordova-plugin-secure-storage.SecureStorage');
 
-console.log("hello world!");
-console.log("dependencies were loaded: " +
-        (typeof(window.sqlitePlugin) !== "undefined" &&
-         typeof(window.sqlitePlugin.openDatabase) === "function"));
-
-var KEYSTORE = "outsystems-key-store"
-var KEYNAME = "outsystems-local-storage-key";
-
-var lsk = "";
-
-function acquireLsk(onSuccess) {
-    var ss = new SecureStorage(
-            function() {
-                ss.get(
-                        function (value) {
-                            lsk = value;
-                            console.log("got lsk:", lsk);
-                            onSuccess(lsk);
-                        },
-                        function (error) {
-                            // recreate lsk
-                            lsk = generateKey();
-                            console.log("setting new lsk");
-                            ss.set(
-                                    function (key) {
-                                        onSuccess(lsk);
-                                    },
-                                    function (error) {
-                                        console.error("error setting local storage key", error);
-                                    },
-                                    KEYNAME, lsk);
-                        },
-                        KEYNAME);
-            },
-            function (error) {
-                console.error("error creating keystore");
-            },
-            KEYSTORE);
+// Validate SQLite plugin API is properly set
+if (typeof(window.sqlitePlugin) === "undefined") {
+	throw new Error("Dependencies were not loaded correctly: SQLite window API is missing.");
 }
 
+if (typeof(window.sqlitePlugin.openDatabase) !== "function") {
+	throw new Error("Dependencies were not loaded correctly: SQLite window API does not provide an `openDatabase` function.");
+}
+
+var OUTSYSTEMS_KEYSTORE = "outsystems-key-store"
+var LOCALSTORAGE_KEY = "outsystems-local-storage-key";
+
+var lskCache = "";
+
+/*
+ * Provides the currently stored Local Storage Key or generates a new one
+ */
+function acquireLsk(successCallback, errorCallback) {
+	// If the key is cached, use it
+	if (lskCache) {
+		successCallback(lskCache);
+		return;
+	}
+	
+	// Otherwise, open the OutSystems key store
+    var ss = new SecureStorage(
+		function () { /* success */ },
+		errorCallback,
+		OUTSYSTEMS_KEYSTORE);
+			
+	// and attempt to get the stored key
+	ss.get(
+		function (value) {
+			lskCache = value;
+			console.log("got lsk:", lskCache);
+			successCallback(lskCache);
+		},
+		function (error) {
+			// If there's no key yet, generate a new one and store it
+			lskCache = generateKey();
+			console.log("setting new lsk");
+			ss.set(
+				function (key) {
+					successCallback(lskCache);
+				},
+				errorCallback,
+				LOCALSTORAGE_KEY, 
+				lskCache);
+		},
+		LOCALSTORAGE_KEY);
+}
+
+/*
+ * Securely generates a new key
+ */
 function generateKey() {
 
     function makeVisible(c) {
-        c+=32;
-        if (c<127) return c;
-        return c+34;
+        c += 32;
+        if (c < 127) return c;
+        return c + 34;
     }
 
     var a = new Uint8Array(16);
@@ -59,17 +74,20 @@ function generateKey() {
             }).join("");
 }
 
-
+// Override existing openDatabase to automatically provide the `key` option
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
-    function innerOpenDb(lsk) {
-        var newOptions = {};
-        for (var key in options) { if (options.hasOwnProperty(key)) newOptions[key] = options[key]; }
-        newOptions["key"] = lsk;
-        return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, errorCallback);
-    }
-    if (lsk) return innerOpenDb(lsk);
-    else {
-        acquireLsk(innerOpenDb);
-    }
+	return acquireLsk(
+		function (key) {
+			var newOptions = {};
+			for (var prop in options) { 
+				if (options.hasOwnProperty(prop)) {
+					newOptions[prop] = options[prop]; 
+				}
+			}
+			newOptions["key"] = key;
+			
+			return originalOpenDatabase.call(window.sqlitePlugin, newOptions, successCallback, errorCallback);
+		},
+		errorCallback);
 }
