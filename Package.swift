537cddd5be4e48e8e9b06c49b553f2d2a6c67748
Rui Mendes <rui.moreira.mendes@outsystems.com>
2026-04-22 12:03:49 +0100

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,36 +1,23 @@
 // swift-tools-version:5.9
 import PackageDescription
 
-// NOTE: Once SPM support is validated and sub-plugins are tagged, replace .branch(...)
-// with .exact("new-version-tag") for each dependency.
 let package = Package(
-    name: "cordova-outsystems-secure-sqlite-bundle",
+    name: "com.outsystems.plugins.SecureSQLiteBundle",
+    platforms: [.iOS(.v14)],
     products: [
         .library(
-            name: "cordova-outsystems-secure-sqlite-bundle",
-            targets: ["cordova-outsystems-secure-sqlite-bundle"]
-        )
+            name: "com.outsystems.plugins.SecureSQLiteBundle",
+            targets: ["com.outsystems.plugins.SecureSQLiteBundle"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/OutSystems/cordova-sqlcipher-adapter.git",
-            branch: "feat/RMET-5136/spm"
-        ),
-        .package(
-            url: "https://github.com/OutSystems/cordova-plugin-secure-storage.git",
-            branch: "feat/RMET-5136/spm"
-        )
+        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master")
     ],
     targets: [
         .target(
-            name: "cordova-outsystems-secure-sqlite-bundle",
+            name: "com.outsystems.plugins.SecureSQLiteBundle",
             dependencies: [
-                .product(name: "cordova-sqlcipher-adapter",
-                         package: "cordova-sqlcipher-adapter"),
-                .product(name: "cordova-plugin-secure-storage",
-                         package: "cordova-plugin-secure-storage")
+                .product(name: "Cordova", package: "cordova-ios")
             ],
-            path: "src/ios"
-        )
+            path: "src/ios")
     ]
 )
