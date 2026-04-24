0e609b32ad7c09846f73702e00d0dc8702c632a4
Rui Mendes <rui.moreira.mendes@outsystems.com>
2026-04-24 16:56:42 +0100

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,17 +10,13 @@ let package = Package(
             targets: ["SecureSQLiteBundlePlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master"),
-        .package(url: "https://github.com/OutSystems/cordova-plugin-secure-storage.git", branch: "feat/RMET-5136/spm"),
-        .package(url: "https://github.com/OutSystems/Cordova-sqlcipher-adapter.git", branch: "feat/RMET-5136/spm")
+        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master")
     ],
     targets: [
         .target(
             name: "SecureSQLiteBundlePlugin",
             dependencies: [
-                .product(name: "Cordova", package: "cordova-ios"),
-                .product(name: "cordova-plugin-secure-storage", package: "cordova-plugin-secure-storage"),
-                .product(name: "cordova-sqlcipher-adapter", package: "cordova-sqlcipher-adapter")
+                .product(name: "Cordova", package: "cordova-ios")
             ],
             path: "src/ios")
     ]
